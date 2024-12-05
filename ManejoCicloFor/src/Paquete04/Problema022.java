20b644540b04e265b50dc7cb1463567b6766a022
unknown <macarrion44@utpl.edu.ec>
2024-12-05 12:07:22 -0500

    Cambios

---
+++ ManejoCicloFor/src/Paquete04/Problema022.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Paquete04;
+
+/**
+ *
+ * @author utpl
+ */
+public class Problema022 {
+    public static void main(String[] args) {
+        int contador;
+        int fahrenheit = 20;
+        double celcius;
+        for (contador = 0; 0<=20; contador++) {
+            celcius = 5(/9
+            System.out.printf("Operación(%d) Fahrenheit: %d ===> Celcius: %.2f";contador,fahrenheit );
+            fahrenheit = fahrenheit + 4;
+            
+        }
+    }
+    
+}
